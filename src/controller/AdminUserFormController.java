7b4275e0218622c3445a15bc1da16d1ded45b147
Sashmitha Ravindu <sashmitharavindu33@gmail.com>
2022-01-25 15:29:38 +0530

    Complete Admin User Form

--- src/controller/AdminUserFormController.java
+++ src/controller/AdminUserFormController.java
@@ -1,5 +1,13 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminUserFormController {
 
+    public AnchorPane adminUserFormContext;
+    public JFXButton btnRecAttendance;
+    public JFXButton btnBackupRestore;
+    public JFXButton btnViewReports;
+    public JFXButton btnManageUsers;
 }
